681c951cf83037dd9ca83dc3b70a75585f5b9f3a
wuxiong <2365603508@qq.com>
2018-10-23 19:20:56 +0800

    vue路由及脚手架

---
+++ vue-demo/src/components/News.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>我是一个新闻</h3>
+        <ul>
+            <li v-for="(val,index) in news" :key="index">{{val}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'news',
+    data(){
+        return {
+            news:['1111','22222','333333','4444444']
+        }
+    }
+}
+</script>
