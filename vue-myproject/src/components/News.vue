4309db92d6149bfabd858964c0bde3d1dbe31c90
wuxiong <2365603508@qq.com>
2018-10-24 17:47:09 +0800

    关于vue与动画结合

---
+++ vue-myproject/src/components/News.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h3>我是一个新闻</h3>
+        <ul>
+            <li v-for="(val,index) in news" :key="index">{{val}}</li>
+        </ul>
+        <ul>
+            <li v-for="(val,index) in tips" :key="index" >{{val}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'news',
+    data(){
+        return {
+            news:['1111','22222','333333','4444444'],
+            tips:['aaa','bbb','ccc','ddd']
+        }
+    }
+}
+</script>
